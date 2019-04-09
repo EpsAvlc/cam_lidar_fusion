81d6697b411ee4601cbf707fe7578f731b71a1ea
Ming Cao <cm9999@qq.com>
2019-04-09 20:42:35 +0800

    update disp_lidar_cam_transform.cpp

--- cam_lidar_fusion/src/point_cloud_proj_node.cpp
+++ cam_lidar_fusion/src/point_cloud_proj_node.cpp
@@ -33,9 +33,9 @@ using namespace cv;
 
 // global paramters;
 string cam_topic_str, lidar_topic_str;
-Mat velo2cam_R, velo2cam_t;
-Mat cam_proj;
+Mat velo2cam;
 Mat rect_R;
+Mat cam_intrins;
 int img_cols, img_rows;
 
 void UpdateParam(ros::NodeHandle nh_local_)
@@ -46,13 +46,13 @@ void UpdateParam(ros::NodeHandle nh_local_)
     FileStorage fs("/home/cm/Workspaces/cam_lidar_fusion/src/cam_lidar_fusion/config/config.yaml", FileStorage::READ);
     ROS_ASSERT(fs.isOpened());
 
-    fs["velo2cam_R"] >> velo2cam_R;
-    fs["velo2cam_t"] >> velo2cam_t;
-    fs["cam_proj"] >> cam_proj;
+    fs["velo2cam"] >> velo2cam;
+    // fs["velo2cam_t"] >> velo2cam_t;
+    fs["cam_intrins"] >> cam_intrins;
     fs["img_cols"] >> img_cols;
     fs["img_rows"] >> img_rows;
-    fs["rect_R"] >> rect_R;
-    ROS_INFO("\n Project information: \n image size: %d X %d \n ", img_cols, img_rows);
+    // fs["rect_R"] >> rect_R;
+    // ROS_INFO("\n Project information: \n image size: %d X %d \n ", img_cols, img_rows);
 }
 /** 
  * output : projected pixel point with distance.
@@ -61,28 +61,27 @@ void UpdateParam(ros::NodeHandle nh_local_)
 void ProjectPointCloudOnImagePlaner(const pcl::PointCloud<pcl::PointXYZ>& transformed_cloud, vector<Point3d>& fit_points)
 {
     fit_points.clear();
-    Eigen::MatrixXd rect_R_eigen(4, 4);
-    cv2eigen(rect_R, rect_R_eigen);
-    Eigen::MatrixXd camera_proj_eigen(3,4);
+    // Eigen::MatrixXd rect_R_eigen(4, 4);
+    // cv2eigen(rect_R, rect_R_eigen);
+    Eigen::Matrix3d camera_intrins_eigen;
     // ROS_INFO("BEFOR EIGENLIZED");
-    cv2eigen(cam_proj, camera_proj_eigen);
+    cv2eigen(cam_intrins, camera_intrins_eigen);
     // ROS_INFO("AFTER EIGENLIZED");
-    Eigen::MatrixXd points(4, transformed_cloud.size());
+    Eigen::MatrixXd points(3, transformed_cloud.size());
     for(int i = 0; i < transformed_cloud.size(); ++i)
     {
         points(0, i) = transformed_cloud[i].x;
         points(1, i) = transformed_cloud[i].y;
         points(2, i) = transformed_cloud[i].z;
-        points(3, i) = 1;
     }
-    Eigen::MatrixXd points_pixel = camera_proj_eigen * rect_R_eigen * points;
+    Eigen::MatrixXd points_pixel = camera_intrins_eigen * points;
     for(int i = 0; i < transformed_cloud.size(); ++i)
     {
         Point3d tmp;
         tmp.x = points_pixel(0, i) / points_pixel(2, i);
         tmp.y = points_pixel(1, i) / points_pixel(2, i);
         tmp.z = points_pixel(2, i);
-
+        // cout << tmp.z << endl;
         if(tmp.x < img_cols && tmp.x > 0 
         && tmp.y < img_rows && tmp.y > 0 && tmp.z > 0)
         {
@@ -95,18 +94,12 @@ void Callback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::PointC
 {
     pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
     pcl::fromROSMsg(*cloud, pcl_cloud);
-    ROS_INFO("Projected_point size: %d", (int)pcl_cloud.size());
-    Eigen::Matrix3d velo2cam_R_eigen;
-    cv2eigen(velo2cam_R, velo2cam_R_eigen);
-    Eigen::Vector3d velo2cam_t_eigen;
-    cv2eigen(velo2cam_t, velo2cam_t_eigen);
-    Eigen::Isometry3d transform;
-    transform.setIdentity(); 
-    transform.rotate(velo2cam_R_eigen);
-    transform.translate(velo2cam_t_eigen);
+
+    Eigen::Matrix4f transform;
+    cv2eigen(velo2cam, transform);
 
     pcl::PointCloud<pcl::PointXYZ> transformed_cloud;
-    pcl::transformPointCloud(pcl_cloud, transformed_cloud, transform.matrix());
+    pcl::transformPointCloud(pcl_cloud, transformed_cloud, transform);
 
     vector<Point3d> proj_points;
     ProjectPointCloudOnImagePlaner(transformed_cloud, proj_points);
@@ -114,16 +107,14 @@ void Callback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::PointC
     // Get cam image
     cv_bridge::CvImagePtr img_ptr = cv_bridge::toCvCopy(image);
     Mat src_img = img_ptr->image;
-    ROS_INFO("Projected_point size: %d", (int)proj_points.size());
-    ROS_INFO("Img channels: %d", src_img.channels());
     for(int i = 0; i < proj_points.size(); ++i)
     {
-        Scalar color(saturate_cast<uchar>(proj_points[i].z / 50. * 255));
+        Scalar color(saturate_cast<uchar>(proj_points[i].z / 3. * 255));
         circle(src_img, Point(proj_points[i].x, proj_points[i].y), 2, color, -1);
     }
     
-    // imshow("after", src_img);
-    // waitKey(5);
+    imshow("after", src_img);
+    waitKey(5);
 }
 
 int main(int argc, char* argv[])
--- cam_lidar_fusion/utils/disp_lidar_cam_transform.cpp
+++ cam_lidar_fusion/utils/disp_lidar_cam_transform.cpp
@@ -10,8 +10,11 @@
 #include <iostream>
 
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 
-#include <opencv2/core/eigen.hpp>
+// #include <opencv2/core/eigen.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 
 using namespace std;
 using namespace cv;
@@ -22,5 +25,21 @@ int main(int argc, char* argv[])
 
     FileStorage fs("/home/cm/Workspaces/cam_lidar_fusion/src/cam_lidar_fusion/config/config.yaml", FileStorage::READ);
 
-    // Mat cam_Lidar_trans
+    Mat lidar2cam;
+    ROS_ASSERT(fs.isOpened());
+    fs["velo2cam"] >> lidar2cam;
+
+    tf::Transform lidar_cam_trans;
+    lidar_cam_trans.setOrigin(tf::Vector3(lidar2cam.at<double>(0,3), lidar2cam.at<double>(1,3), lidar2cam.at<double>(2,3)));
+    // cout << tf::Vector3(lidar2cam.at<double>(3,0), lidar2cam.at<double>(3,1), lidar2cam.at<double>(3,2)); 
+    tf::Matrix3x3 lidar_cam_rot;
+    lidar_cam_rot.setValue(lidar2cam.at<double>(0,0), lidar2cam.at<double>(0,1), lidar2cam.at<double>(0,2),
+                        lidar2cam.at<double>(1,0), lidar2cam.at<double>(1,1), lidar2cam.at<double>(1,2),
+                        lidar2cam.at<double>(2,0), lidar2cam.at<double>(2,1), lidar2cam.at<double>(2,2));
+    tf::Quaternion lidar_cam_q;
+    lidar_cam_rot.getRotation(lidar_cam_q);
+    lidar_cam_trans.setRotation(lidar_cam_q);
+    tf::TransformBroadcaster br;
+    while (ros::ok()) 
+        br.sendTransform(tf::StampedTransform(lidar_cam_trans, ros::Time::now(), "camera", "lidar"));
 }
